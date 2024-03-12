cbe31509757616cac5733a06699b9c5e118a71fe
michiaal <misha5026@ukr.net>
2024-03-12 19:22:56 +0200

    add part  off JS homework

---
+++ Homeworks/js/index.js
@@ -0,0 +1,63 @@
+
+
+//* 1 вправа
+
+ let username = prompt ('Як тебе звати');
+  alert("привіт ім'я");
+   console.log(username);
+
+//* 2 вправа
+
+const YEAR = ('2024')
+ const birthyear = prompt ('What year were you born? ?');
+  const result = alert (`Вам ${YEAR-birthyear} р`);
+   console.log ('Вам:',YEAR-birthyear);
+  
+
+//* 3 вправа
+
+let question = prompt ('Яка довжина сторони квадрату');
+ const x = 4;
+   const end = alert(` периметр квадрату ${question*x} `);
+
+
+//* 4 вправа
+
+ let faq = prompt ('Який радіус кола ?');
+  let degree = (faq ** 2);
+   const P = (3.14) ;
+    let action = (degree * P) ;
+     console.log(action);
+
+
+
+
+
+
+
+
+
+// let admin, name ;
+// name = 'Іван';
+// admin = name ;5
+// alert(admin);
+
+// const massage = 'hi';
+
+// console.log(massage);
+
+
+// console.log(massage);
+
+
+// alert(massage)
+
+
+
+
+ //console.log(massage);
+
+/*alert()
+confirm()
+prompt()*/
+
