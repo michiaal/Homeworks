048abf04feeddb82904639233e23a3aa4c08ab46
michiaal <misha5026@ukr.net>
2024-03-12 20:48:15 +0200

    rework JS

--- Homeworks/js/index.js
+++ Homeworks/js/index.js
@@ -1,24 +1,24 @@
 
 
-//* 1 вправа
+// //* 1 вправа
 
- let username = prompt ('Як тебе звати');
-  alert("привіт ім'я");
-   console.log(username);
+//  let username = prompt ('Як тебе звати');
+//   alert("привіт ім'я");
+//    console.log(username);
 
-//* 2 вправа
+// //* 2 вправа
 
-const YEAR = ('2024')
- const birthyear = prompt ('What year were you born? ?');
-  const result = alert (`Вам ${YEAR-birthyear} р`);
-   console.log ('Вам:',YEAR-birthyear);
+// const YEAR = ('2024')
+//  const birthyear = prompt ('What year were you born? ?');
+//   const result = alert (`Вам ${YEAR-birthyear} р`);
+//    console.log ('Вам:',YEAR-birthyear);
   
 
-//* 3 вправа
+// //* 3 вправа
 
-let question = prompt ('Яка довжина сторони квадрату');
- const x = 4;
-   const end = alert(` периметр квадрату ${question*x} `);
+// let question = prompt ('Яка довжина сторони квадрату');
+//  const x = 4;
+//    const end = alert(` периметр квадрату ${question*x} `);
 
 
 //* 4 вправа
@@ -27,6 +27,7 @@ let question = prompt ('Яка довжина сторони квадрату');
   let degree = (faq ** 2);
    const P = (3.14) ;
     let action = (degree * P) ;
+     alert (action);
      console.log(action);
 
 
