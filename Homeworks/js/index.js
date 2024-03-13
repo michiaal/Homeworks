17c2b768c76f4d164466d6440cc147e96506399c
michiaal <misha5026@ukr.net>
2024-03-13 08:33:16 +0200

    remove coments

--- Homeworks/js/index.js
+++ Homeworks/js/index.js
@@ -1,50 +1,50 @@
 
 
-// //* 1 вправа
+//* 1 вправа
 
-//  let username = prompt ('Як тебе звати');
-//   alert("привіт ім'я");
-//    console.log(username);
+ let username = prompt ('Як тебе звати');
+  alert("привіт ім'я");
+   console.log(username);
 
-// //* 2 вправа
+//* 2 вправа
 
-// const YEAR = ('2024')
-//  const birthyear = prompt ('What year were you born? ?');
-//   const result = alert (`Вам ${YEAR-birthyear} р`);
-//    console.log ('Вам:',YEAR-birthyear);
+const YEAR = ('2024')
+ const birthyear = prompt ('What year were you born? ?');
+  const result = alert (`Вам ${YEAR-birthyear} р`);
+   console.log ('Вам:',YEAR-birthyear);
   
 
-// //* 3 вправа
+//* 3 вправа
 
-// let question = prompt ('Яка довжина сторони квадрату');
-//  const x = 4;
-//    const end = alert(` периметр квадрату ${question*x} `);
+let question = prompt ('Яка довжина сторони квадрату');
+ const x = 4;
+   const end = alert(` периметр квадрату ${question*x} `);
 
 
-// //* 4 вправа
+//* 4 вправа
 
-//  let faq = prompt ('Який радіус кола ?');
-//   let degree = (faq ** 2);
-//    const P = (3.14) ;
-//     let action = (degree * P) ;
-//      alert (action);
-//      console.log(action);
+ let faq = prompt ('Який радіус кола ?');
+  let degree = (faq ** 2);
+   const P = (3.14) ;
+    let action = (degree * P) ;
+     alert (action);
+     console.log(action);
 
-// //* 5 вправа
-// let question1 = prompt ('Яка відстань ?') ;
-//  let question2 = prompt ('Скільки годин') ;
-//   let resultt = (question1/question2) ;
-//    alert(resultt);
-//     console.log(resultt);
+//* 5 вправа
+let question1 = prompt ('Яка відстань ?') ;
+ let question2 = prompt ('Скільки годин') ;
+  let resultt = (question1/question2) ;
+   alert(resultt);
+    console.log(resultt);
 
-// //* 6 вправа 
+//* 6 вправа 
 
-// const EURO = ('0.92')
-//  const DOLLAR = ('1')
-//   let value = prompt ('Скіко грошів ?')
-//    let sum = (value * EURO);
-//     alert(sum)
-//      console.log(sum)
+const EURO = ('0.92')
+ const DOLLAR = ('1')
+  let value = prompt ('Скіко грошів ?')
+   let sum = (value * EURO);
+    alert(sum)
+     console.log(sum)
 
 
 
